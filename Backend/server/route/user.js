3f0d549140b92495687488207f0739f2ed3212d7
Lawson <abrahamlawson67@gmail.com>
2023-11-29 17:41:18 +0100

    Create route contacUsers

--- Backend/server/route/user.js
+++ Backend/server/route/user.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = new express.Router();
-const connection = require("../config/db")
+const connection = require("../config/db");
 
 // import controller user
-const {signupUsers , loginUsers } = require('../controllers/userController')
+const { signupUsers, loginUsers } = require("../controllers/userController");
 
 // login route
-router.post('/login',loginUsers)
+router.post("/login", loginUsers);
 
 // Signup route
-router.post('/signup',signupUsers)
+router.post("/signup", signupUsers);
+router.post("/contact", contactUsers);
 
 module.exports = router;
